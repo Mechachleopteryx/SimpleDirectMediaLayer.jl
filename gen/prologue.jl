ce9186ac2af9132c575baacbd480fbde87b54313
Gnimuc <qiyupei@gmail.com>
2021-02-26 22:22:44 +0900

    Add generator script(use Clang.jl's new generator)

---
+++ gen/prologue.jl
@@ -0,0 +1,10 @@
+using SDL2_jll
+export SDL2_jll
+using SDL2_mixer_jll
+export SDL2_mixer_jll
+using SDL2_image_jll
+export SDL2_image_jll
+using SDL2_ttf_jll
+export SDL2_ttf_jll
+
+const SDL_BlitMap = Cvoid
